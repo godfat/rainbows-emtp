42a64fb07a3c4ca461bcfa9fde01d4308926ff79
Lin Jen-Shin <godfat@godfat.org>
2013-08-23 06:36:43 +0800

    autoload :Client

--- lib/rainbows/event_machine_thread_pool.rb
+++ lib/rainbows/event_machine_thread_pool.rb
@@ -2,7 +2,14 @@
 require 'rainbows'
 
 module Rainbows::EventMachineThreadPool
+  autoload :Client, 'rainbows/event_machine_thread_pool/client'
+  extend  Rainbows::PoolSize
   include Rainbows::EventMachine
+
+  def init_worker_process worker
+    EM.threadpool_size = Rainbows::O[:pool_size]
+    super
+  end
 
   def worker_loop worker
     server = Rainbows.server
